77536e5a8844fab5e883cc294d040ff973ce5b09
DanteMedici <dmedici@frba.utn.edu.ar>
2025-09-07 23:54:32 +0000

    Logs minimos y obligatorios y storage config
    Co-authored-by: Federico Iannino <fiannino@frba.utn.edu.ar>
    Co-authored-by: Candela-nieva <Candela-nieva@users.noreply.github.com>
    Co-authored-by: Nara Santana  <nsantanadasilva@frba.utn.edu.ar>
    Co-authored-by: Ezequiel Reichel <Reichel-eze@users.noreply.github.com>

--- master/src/main.c
+++ master/src/main.c
@@ -1,29 +1,5 @@
 
 #include "masterUtils.h"
-void* atender_conexion(void* arg);
-void atender_QueryControl(int fd);
-void atender_Worker(int fd);
-void* atender_conexion(void* arg){
-    int fd = *(int *)arg;
-    free(arg);
-    op_code op = recibir_operacion(fd);
-    if (op == HANDSHAKE_QUERY)
-    {
-        log_info(loggerMaster, "## Query Control Conectado - FD del socket: %d", fd);
-        atender_QueryControl(fd);
-    }
-    else if (op == HANDSHAKE_WORKER)
-    {
-        log_info(loggerMaster, "## Worker Conectado - FD del socket: %d", fd);
-        atender_Worker(fd);
-    }
-    else
-    {
-        log_info(loggerMaster, "## Handshake inválido (%d) en fd %d", op, fd);
-    }
-    close(fd);
-    return NULL;
-}
 
 int main(int argc, char* argv[]) {
     saludar("master");
@@ -37,40 +13,11 @@ int main(int argc, char* argv[]) {
     {
         int *peticion = malloc(sizeof(int));
         *peticion = esperar_cliente(fd_sv, "MASTER", loggerMaster);
-
+        
         pthread_t tid;
         pthread_create(&tid, NULL, atender_conexion, peticion);
         pthread_detach(tid);
     }
-    // Nunca llega acá
     close(fd_sv);
     return 0;
-}
-
-void atender_QueryControl(int fd){
-    log_info(loggerMaster, "CONEXION EXITOSA CON QUERYCONTROL");
-    int length_path, prioridad;
-    int offset = 0;
-    char* path_query;
-    void* buffer = recibir_buffer(fd);
-    memcpy(&length_path,buffer + offset, sizeof(int));
-    offset += sizeof(int);
-    path_query = malloc(length_path + 1);
-
-    memcpy(path_query, buffer + offset, length_path);
-    path_query[length_path] = '\0';
-    offset += length_path;
-
-    memcpy(&prioridad, buffer + offset, sizeof(int));
-    free(buffer);
-    log_info(loggerMaster, "Query recibido; PATH = %s ; PRIORIDAD = %d", path_query, prioridad);
-}
-
-void atender_Worker(int fd){
-    log_info(loggerMaster, "CONEXION EXITOSA CON WORKER");
-    int id_worker;
-    void* buffer = recibir_buffer(fd);
-    memcpy(&id_worker, buffer, sizeof(int));
-    free(buffer);
-    log_info(loggerMaster,"Worker Conectado, ID : %d",id_worker);
 }
